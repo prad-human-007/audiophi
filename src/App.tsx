7c9b78c5696be51ed91ee3d67f172bda337d25f4
prad-human-007 <kanasepradhumn@gmail.com>
2025-01-19 12:50:29 +0530

    Created the basic vite project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <div className='flex flex-col gap-4 w-full h-screen justify-center items-center '>
+      <h1> Audiophi </h1>
+      <button>Hello</button>
+    </div>
+  )
+}
+
